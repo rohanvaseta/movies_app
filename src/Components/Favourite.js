c4588426759938a0b110737a8ade263035c268fa
rohan vaseta <rohanvaseta81@gmail.com>
2023-04-12 20:42:24 +0530

    fav butto2n

--- src/Components/Favourite.js
+++ src/Components/Favourite.js
@@ -15,7 +15,7 @@ return (
   <div  className="w-72 h-108 flex flex-wrap justify-center items-center hover:bg-gray-400 rounded-md">
   <img className="inline w-64 h-68 mt-3 mb-0" src={`https://image.tmdb.org/t/p/w500/${val.poster_path}`} alt="" />
   <div className='w-48'><h3 className="">{val.title}</h3>
-  <button onClick={()=>props.removeFavoriteMovie(val)} className='bg-red-200 rounded-lg px-2'>Remove &hearts;</button>
+  <button onClick={()=>props.removeFavouriteMovie(val)} className='bg-red-200 rounded-lg px-2 '>Remove &hearts;</button>
   </div>
  
 </div>
