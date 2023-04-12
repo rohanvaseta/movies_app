b045556a487c99a1e48a889fab6f21cbf8d9d615
rohan vaseta <rohanvaseta81@gmail.com>
2023-04-12 20:37:08 +0530

    fav butto2n

--- src/Components/TreandingMovies.js
+++ src/Components/TreandingMovies.js
@@ -11,7 +11,7 @@ const TreandingMovies = (props) => {
 return (
   <div  className="w-72 h-108 flex flex-wrap justify-center items-center hover:bg-gray-400 rounded-md">
   <img className="inline w-64 h-68 mt-3 mb-0" src={`https://image.tmdb.org/t/p/w500/${val.poster_path}`} alt="" />
-  <div className='w-48'><h3>{val.title}</h3><h3>{val.name}</h3>
+  <div className='w-48'><h3>{val.title}</h3>
   <button onClick={()=>props.addFavoriteMovie(val)} className='bg-red-200 rounded-lg px-2'>Add To fav &hearts;</button>
   </div>
  
